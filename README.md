5f7de3c0b17883a1a3927f9017ff1ddbaa2e8b07
Rafeed Mahbub Rafi <83001224+rafi99-bat@users.noreply.github.com>
2021-07-07 00:31:30 +0600

    Added README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Realm
+
+This is a retro massive multiplayer online game from scratch in Java (Java SE 1.8). Basically it is kind of a [Realm of the Mad God](https://www.realmofthemadgod.com/) clone and it will feature both single player and multiplayer.
--- src/com/project/mmo/Game.java
+++ src/com/project/mmo/Game.java
@@ -1,7 +1,11 @@
 package com.project.mmo;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+
 import javax.swing.JFrame;
 
 public class Game extends Canvas implements Runnable{
@@ -39,8 +43,27 @@ public class Game extends Canvas implements Runnable{
 	
 	public void run() {
 		while(running) {
-			System.out.println("Running....");
+			update();
+			render();
 		}
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void render() {
+		BufferStrategy bs = getBufferStrategy();
+		if (bs == null) {
+			createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		g.dispose();
+		bs.show();
 	}
 	
 	public static void main(String[] args) {
